be9591e62d8c4fc2a695d2016dbdeffbae963029
Leonardo Goulart <leonardogoulartt1@gmail.com>
2017-11-23 11:03:26 -0200

    crud done

--- dashboard/models/client.py
+++ dashboard/models/client.py
@@ -6,7 +6,11 @@ class Client(models.Model):
     owner = models.ForeignKey('auth.User')
     first_name = models.CharField(max_length=100, null=False)
     last_name = models.CharField(max_length=200, null=False)
-    cpf = models.IntegerField(null=True, blank=True)
+    cpf = models.BigIntegerField(null=True, blank=True)
+    primary_phone_number = models.CharField(max_length=15, blank=True)
+    secondary_phone_number = models.CharField(max_length=15, blank=True)
+    email = models.CharField(max_length=200, blank=True)
+    address = models.CharField(max_length=200, blank=True)
     created_at = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
